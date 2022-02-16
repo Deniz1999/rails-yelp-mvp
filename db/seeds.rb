f231768d4b5cd12a93728200cdd1987a29ef0940
Deniz Kazdal <denizkazdal99@gmail.com>
2022-02-16 15:07:16 +0000

    Completed Rails Yelo MVP

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,15 @@
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
-#
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+Restaurant.destroy_all
+
+restaurant = Restaurant.new(name: Faker::Name.name, address:Faker::Address.city, phone_number: "07500524881", category: "belgian")
+restaurant.save!
+
+review = Review.new(content: 'yummy', rating: 5)
+review.restaurant = restaurant
+review.save!
